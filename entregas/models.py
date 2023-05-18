3a890c95f826c17c68bf3486fd6cb3189ffede38
Sandra <sandra_silva_santos@live.com>
2023-05-17 23:06:40 -0300

    Update

--- entregas/models.py
+++ entregas/models.py
@@ -2,32 +2,31 @@ from django.db import models
 from django.urls import reverse
 import uuid
 
-class Entrega(models.Model):
-    ENTREGUE = 'E'
-    PENDENTE = 'P'
-    STATUS_CHOICES = [
-        (ENTREGUE, 'Entregue'),
-        (PENDENTE, 'Pendente'),
-    ]
-    
-    
+
+
+class Entrega(models.Model):       
     id = models.CharField(primary_key=True, default=uuid.uuid4, editable=False, max_length=36)
     nome = models.CharField(max_length=255)
     bloco = models.IntegerField()
     andar = models.IntegerField()
     apartamento = models.IntegerField()
     email = models.EmailField()
-    status = models.CharField(choices=STATUS_CHOICES, max_length=10)
     data_entrega = models.DateTimeField(auto_now_add=True)
-    entregue = models.BooleanField(default=False)
+    porteiro = models.ForeignKey('Porteiro', on_delete=models.CASCADE, null=True, blank=True)
+    morador = models.ForeignKey('Morador', on_delete=models.CASCADE, null=True, blank=True)
+
+    STATUS_CHOICES = (
+        ('P', 'Pendente'),
+        ('E', 'Entregue'),
+        ('A', 'Aguardando Retirada'),
+    )
+
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='P')
+
+
 
     def __str__(self):
         return self.nome
-
-    def get_absolute_url(self):
-        return reverse('entrega-detail', kwargs={'pk': self.pk})
-
-
 
 class Morador(models.Model):
     id = models.CharField(primary_key=True, default=uuid.uuid4, editable=False, max_length=36)
@@ -37,7 +36,17 @@ class Morador(models.Model):
     andar = models.IntegerField()
     email = models.EmailField()
 
+    def __str__(self):
+        return self.nome
 
+class Porteiro(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    senha = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nome
+
+
+
+
